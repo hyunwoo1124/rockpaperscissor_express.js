4ea9e0f8dc2ed37aac06f231c077d6030d942bdd
kevin <hyunwoo777@csu.fullerton.edu>
2020-03-27 15:55:34 -0700

    comments and formatting

--- backend.js
+++ backend.js
@@ -1,11 +1,7 @@
-//"use strict" This is only required for objects on the actual rpc game to "seal object game"
+"use strict"
 const express = require('express');
 const app = express();
-//const server = require('http').createServer(app); dont need to use http since we are using 'express'
-//const io = require('socket.io').listen(server);   Rule did say not to use any packages other than his lectures
-/*
-Newly added -kevin-
-*/
+
 // post method is stored in the body so parser is necessary to handle post request
 const bodyParser = require("body-parser");
 // Allow extended to change/modify objects (may have to change later depending how our prg goes)
@@ -13,97 +9,134 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs');
 
-// Handles HTML request of the url
+// class rps
+const rps = class
+{
+    constructor(listen, computerScore, humanScore, tieScore, totalGame, gameResult, computerChoice) 
+    {
+        this.listen = 0
+        this.computerScore = computerScore = 0;
+        this.humanScore = humanScore = 0;
+        this.tieScore = tieScore = 0;
+        this.totalGame = totalGame = 0;
+        this.gameResult = gameResult;
+        this.computerChoice = computerChoice=0;
+    }
+}
+// rps prototype to compare radio button(index.html) with anonymous function
+rps.prototype.compChoice = function() 
+{
+    this.computerChoice = Math.floor(Math.random() * 3);
+
+    if (this.computerChoice === 0) 
+    {
+        this.computerChoice = "Rock";
+        return this.computerChoice;
+    } 
+    else if (this.computerChoice === 1) 
+    {
+        this.computerChoice = "Paper";
+        return this.computerChoice;
+    } 
+    else if (this.computerChoice === 2) 
+    {
+        this.computerChoice = "Scissors";
+        return this.computerChoice;
+    }
+    console.log("Computer uses: " + this.computerChoice);
+}
+
+// Creating instance of a class rps -> rpsObj
+let rpsObj= new rps(); 
+
+
+// function prototype to compare player and computer choice
+rps.prototype.compare = function(playersChoice, computerChoice)
+{
+    if (playersChoice === computerChoice) 
+    {
+        console.log("Tie");
+        this.tieScore++;
+        this.gameResult = "Tie!";
+    } 
+    else if (playersChoice === 'Rock') 
+    {
+        if (computerChoice === 'Paper') 
+        {
+            console.log("Computer Wins");
+            this.computerScore++;
+            this.gameResult = "You lose!";
+        } 
+        else 
+        {
+            console.log("Player wins");
+            this.humanScore++;
+            this.gameResult = "You win!";
+        }
+    } 
+    else if (playersChoice === 'Paper') 
+    {
+        if (computerChoice === 'Scissors') 
+        {
+            console.log("Computer Wins");
+            this.computerScore++;
+            this.gameResult = "You lose!";
+        } 
+        else 
+        {
+            console.log("Player wins");
+           this. humanScore++;
+           this.gameResult = "You win!";
+        }
+    } 
+    else if (playersChoice === 'Scissors') 
+    {
+        if (computerChoice === 'Rock') 
+        {
+            console.log("Computer Wins");
+            this.computerScore++;
+            this.gameResult = "You lose!";
+        }
+        else 
+        {
+            console.log("Player wins");
+            this.humanScore++;
+            this.gameResult = "You win!";
+        }
+    }
+
+    this.totalGame++;
+
+    console.log("Human: ", this.humanScore);
+    console.log("Computer: ", this.computerScore);
+    console.log("Ties: ", this.tieScore);
+    console.log("Total Score: ", this.totalGame);
+}
+
+// get method of index.html 
 app.get('/', function(req, res)
 {
     res.sendFile(__dirname + '/index.html');
 });
-
-app.post('/mygame', function(req, res){
+// post method to body parse and receive
+app.post('/mygame', function(req, res)
+{
     console.log("Got data: " + req.body.choice);
- //   console.log("Got data: " + req.body.Paper);
-   // console.log("Got data: " + req.body.Scissor);
-    
-    
-    // gets the players choice - jason 3/20
+   
     let playersChoice = req.body.choice;
+    // sealing playerchoice objects to disable add/delete 
     Object.seal(playersChoice);
-    // random choice between 0 and 3 to determine computers choice - jason 3/20
-    let computerChoice = Math.floor(Math.random() * 3);
-    Object.seal(computerChoice);
-    if (computerChoice == 0)
-    {
-        computerChoice = "Rock";
-    }else if (computerChoice == 1)
-    {
-        computerChoice = "Paper";
-    } else if (computerChoice == 2)
-    {
-        computerChoice = "Scissors";
-    }
-    console.log("Computer uses: " + computerChoice);
-// comparing who wins- jason 3/20
-    compare(playersChoice,computerChoice);
-    // keivn 3/24
-    // made game.ejs to dynamically generate pages, require work to redirect to the main page again
-    res.render('game', {gameResult: gameResult, player: playersChoice, server: computerChoice, playerScore: humanScore, serverScore:computerScore, totalScore: totalGame, tieScore: tieScore});
+
+    // sealing obj of instance of a class named rps
+    let obj = Object.seal(rps);
     
-   // res.redirect('/');
+    let computer= rpsObj.compChoice();
+
+    rpsObj.compare(playersChoice, computer);
+
+    // ejs dynamic page generation with newly updated scores every time game ends
+    res.render('game', {gameResult: rpsObj.gameResult, player: playersChoice, server: rpsObj.computerChoice, playerScore: rpsObj.humanScore, serverScore:rpsObj.computerScore, totalScore: rpsObj.totalGame, tieScore: rpsObj.tieScore});
+
 })
 
-
 app.listen(3000);
-
-let computerScore = 0;
-let humanScore = 0;
-let tieScore = 0;
-let totalGame = 0;
-let gameResult = "Tie"
-
-// compares the choices - jason 3/20
-let compare = function(playersChoice, computerChoice) {
-    "use strict"
-    if (playersChoice === computerChoice) {
-        console.log("Tie");
-        tieScore++;
-        gameResult = "Tie!";
-    }
-    else if (playersChoice === 'Rock') {
-        if (computerChoice === 'Paper') {
-            console.log("Computer Wins");
-            computerScore++;
-            gameResult = "You lose!";
-        } else {
-            console.log("Player wins");
-            humanScore++;
-            gameResult = "You win!";
-        }
-    }
-    else if (playersChoice === 'Paper') {
-        if (computerChoice === 'Scissors') {
-            console.log("Computer Wins");
-            computerScore ++;
-            gameResult = "You lose!";
-        } else {
-            console.log("Player wins");
-            humanScore++;
-            gameResult = "You win!";
-        }
-    }
-    else if (playersChoice === 'Scissors') {
-        if (computerChoice === 'Rock') {
-            console.log("Computer Wins");
-            computerScore ++;
-            gameResult = "You lose!";
-        } else {
-            console.log("Player wins");
-            humanScore++;
-            gameResult = "You win!";
-        }
-    }
-    totalGame++;
-    console.log("Human: ", humanScore );
-    console.log("Computer: ", computerScore);
-    console.log("Ties: ", tieScore);
-    console.log("Total Score: ", totalGame);
-}
